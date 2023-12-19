e60076f187bff7c7d5d7e47577b165577fae2d2f
Laudial <laudial.dev@gmail.com>
2023-12-19 11:18:53 +0400

    Update

--- main.go
+++ main.go
@@ -1,2 +1,23 @@
 package main
 
+import (
+	"fmt"
+
+	"WebScraping/controllers"
+	"WebScraping/utils"
+)
+
+func main() {
+
+	var (
+		link string
+	)
+
+	// Nettoyer le terlminale
+	utils.ClearScreen()
+
+	fmt.Print("Site à scraper : ")
+    fmt.Scan(&link)
+
+	controllers.StartScraping(link)
+}
